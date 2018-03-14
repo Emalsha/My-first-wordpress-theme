958d07759aa955d8e25d76649d3b7e61ab773793
emalsha <remalsha@gmail.com>
2018-03-14 23:54:40 +0530

    something

--- template-parts/post/content-big-five.php
+++ template-parts/post/content-big-five.php
@@ -20,7 +20,7 @@ $custom_fields = get_post_custom();
 <div class="p-3 col-md-6 ">
     <div class="border border-dark rounded">
         <div class="row ">
-            <div class="col-md-6">
+            <div class="col-md-6 pr-0">
                 <?php if ('' !== get_the_post_thumbnail() && !is_single() && !get_post_gallery()) : ?>
                     <?php the_post_thumbnail('wanabima-featured-image', ['class' => 'img-fluid']); ?>
                 <?php endif; ?>
@@ -30,7 +30,8 @@ $custom_fields = get_post_custom();
                     <h3 class="card-title"><?php echo get_the_title(); ?></h3>
                     <h5 class="card-title">Big Five With Wanabima</h5>
                     <?php the_content(); ?>
-<!--                    <p class="card-text">--><?php //the_content(); ?><!--</p>-->
+<!--                    <p class="car
+d-text">--><?php //the_content(); ?><!--</p>-->
                     <a href="<?php the_permalink(); ?>" class="btn btn-outline-dark"><?php echo $custom_fields['BUTTON'] ? $custom_fields['BUTTON'][0] : "MORE" ?></a>
                 </div>
             </div>
