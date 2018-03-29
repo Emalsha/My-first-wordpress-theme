c39c20da7f64a73e3e737b4f97be76b9592a530d
emalsha <remalsha@gmail.com>
2018-03-29 20:39:26 +0530

    archive page update

--- archive-resortandvilla.php
+++ archive-resortandvilla.php
@@ -7,7 +7,12 @@
  * @author Emalsha Rasad
  */
 
-get_header(); ?>
+get_header();
+
+$cpage = 'resortandvilla';
+
+
+?>
 
     <main id="main">
 
@@ -21,7 +26,6 @@ get_header(); ?>
                     <?php
                     global $wpdb;
 
-                    $cpage = 'resortandvilla';
                     $position = 1;
 
                     $content = $wpdb->get_row("SELECT * FROM " . $wpdb->prefix . "customcontent WHERE text_page='$cpage' AND text_page_position='$position'");
@@ -81,7 +85,6 @@ get_header(); ?>
         <?php
         global $wpdb;
 
-        $cpage = 'resortandvilla';
         $position = 2;
 
         $content = $wpdb->get_row("SELECT * FROM " . $wpdb->prefix . "customcontent WHERE text_page='$cpage' AND text_page_position='$position'");
@@ -103,6 +106,85 @@ get_header(); ?>
             <?php
         }
         ?>
+
+<!--==========================
+  Featured Tours Section
+============================-->
+        <section id="featured-tour" class="wow fadeIn">
+            <div class="container-fluid text-center">
+                <h3 class="h3">Featured Tours</h3>
+
+                <div class="row">
+                    <?php
+                    global $wpdb;
+
+                    $fimages = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "feature_image WHERE feature_page='$cpage' ORDER BY 'feature_image_position'");
+                    if ($fimages) {
+                        foreach ($fimages as $fimage) {
+                            ?>
+                            <div class="col-lg-3 col-md-6 wow fadeInUp">
+                                <a href="<?php echo $fimage->url; ?>">
+                                    <div class="feature-item">
+                                        <!--Use 683 x 1024 image -->
+                                        <?php $attachmentImg = wp_get_attachment_image_src($fimage->feature_image, full);
+                                        if ($attachmentImg) {
+                                            ?>
+                                            <img src="<?php echo $attachmentImg[0] ?>" class="img-fluid"
+                                                 alt="Wanabima feature tours">
+                                            <?php
+                                        } else {
+                                            ?>
+                                            <img src="<?php echo get_template_directory_uri(); ?>/img/wanabima%20683x1024.png" class="img-fluid"
+                                                 alt="Wanabima feature tours">
+                                            <?php
+                                        }
+                                        ?>
+                                        <div class="overlay feature-item-info">
+                                            <div class="feature-item-info-content">
+                                                <h4><?php echo $fimage->feature_title; ?></h4>
+                                                <span><?php echo $fimage->feature_sub_title; ?></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <?php
+                        }
+                    }
+                    ?>
+
+
+                </div>
+            </div>
+        </section><!-- #Feature Tours section -->
+
+
+        <?php
+        global $wpdb;
+
+        $position = 3;
+
+        $content = $wpdb->get_row("SELECT * FROM " . $wpdb->prefix . "customcontent WHERE text_page='$cpage' AND text_page_position='$position'");
+        if ($content) {
+            ?>
+            <!--==========================
+                     Content 3 Section
+                   ============================-->
+            <section id="content3">
+                <div class="container">
+
+                    <header class="section-header">
+                        <h3><?php echo $content->text_title; ?></h3>
+                        <h4 class="text-center"><?php echo $content->text_sub_title; ?></h4>
+                        <p><?php echo $content->text; ?></p>
+                    </header>
+                </div>
+            </section>
+            <?php
+        }
+        ?>
+
     </main><!-- .main-->
 
 <?php get_footer();
